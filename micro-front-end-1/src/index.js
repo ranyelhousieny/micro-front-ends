bda1a820fb54d230e3fc1244c44dae8acddf6601
Rany Elhousieny <ranyel@msn.com>
2021-02-09 12:36:07 -0800

    step 4 add Lorem Ipsum to MF1

--- micro-front-end-1/src/index.js
+++ micro-front-end-1/src/index.js
@@ -1,1 +1,8 @@
-document.querySelector('#root').innerHTML = `<h1>Micro-Front-End-1</h1>`
+document.querySelector('#root').innerHTML = 
+    `<h1>Micro-Front-End-1</h1>
+    <p>This is the first Micro Frontend</p>
+    <p> Fugiat exercitation irure in ex. Ut sunt qui nostrud sit nostrud aliquip minim excepteur irure tempor aute aute ad dolor. Sit elit consectetur ullamco dolor.
+
+Incididunt tempor consectetur sint est. Eu incididunt in elit eiusmod in consectetur aliquip occaecat ipsum mollit commodo qui nulla. Ipsum Lorem minim magna tempor do nostrud enim dolor dolore laborum fugiat aliquip et. Duis esse nulla in ut cupidatat id. Commodo incididunt ex occaecat sunt fugiat dolor aliqua nostrud eiusmod dolor dolore commodo nulla fugiat. Est aute sunt excepteur proident eiusmod id officia quis. Officia non ea pariatur ea duis fugiat mollit sit duis.
+
+Duis velit pariatur nostrud aliqua laboris irure amet Lorem amet. Eiusmod enim aliquip et ea ea quis irure occaecat qui in et nostrud. Elit est officia adipisicing qui fugiat sint non elit nisi tempor. Sint deserunt proident magna laboris esse velit mollit irure esse sint laborum eu veniam aliquip. Anim eu ipsum aliquip ullamco excepteur eu esse enim minim adipisicing nostrud magna veniam. Pariatur mollit ullamco commodo ea duis duis. Eu deserunt proident mollit aliqua nostrud voluptate consectetur Lorem nisi et qui sunt et deserunt.</p>`
